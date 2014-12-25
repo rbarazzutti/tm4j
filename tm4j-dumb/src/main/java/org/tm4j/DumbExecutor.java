4d5f45ece158ce2930924adbb175064ccd7f4b9d
Raphaël P. Barazzutti <raphael.barazzutti@unine.ch>
2014-12-25 09:49:36 +0100

    added needed stats to DumbExecutor

--- tm4j-dumb/src/main/java/org/tm4j/DumbExecutor.java
+++ tm4j-dumb/src/main/java/org/tm4j/DumbExecutor.java
@@ -3,9 +3,17 @@ package org.tm4j;
 
 import java.util.concurrent.Callable;
 
+/**
+ * Dumb (and awful) TMExecutor which fulfills the atomicity requirements using a central lock. This
+ * implementations can only be used for simple test cases and/or in rescue situation.
+ *
+ * @see org.tm4j.TMExecutor
+ */
 public class DumbExecutor implements TMExecutor {
+    private long serialCount=0;
     public <T> T execute(Callable<T> c) throws Exception {
         synchronized (this) {
+            serialCount++;
             return c.call();
         }
     }
@@ -20,5 +28,24 @@ public class DumbExecutor implements TMExecutor {
 
     public boolean isSupported() {
         return true;
+    }
+
+    public TMStats getStats(){
+        return new TMStats() {
+            @Override
+            public long getSerialCommitsCount() {
+                return serialCount;
+            }
+
+            @Override
+            public long getTransactionalCommitsCount() {
+                return 0;
+            }
+
+            @Override
+            public long getAbortsCount() {
+                return 0;
+            }
+        };
     }
 }
