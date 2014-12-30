56657c80044044cb68e45ccded738d305fb37b4f
Patrick Marlier <patrick.marlier@gmail.com>
2014-12-30 12:06:19 +0100

    Fix formatting

--- tm4j-tsx/src/main/java/org/tm4j/TSXExecutor.java
+++ tm4j-tsx/src/main/java/org/tm4j/TSXExecutor.java
@@ -5,10 +5,9 @@ import java.util.concurrent.Callable;
 public class TSXExecutor extends TSXJNI implements TMExecutor {
 
 
-    public  <V> V execute(Callable<V> c, TMContext context ){
+    public <V> V execute(Callable<V> c, TMContext context) {
         return execute(c, context.getMaxNumberOfRetries());
     }
-
 
     public boolean isSupported() {
         return isLoaded() && hasRTMSupport();
